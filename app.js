ff7eb7f2d374f8833bad7d4b3a0ad80dff23686f
Hodor <hodor@Manufactons-MacBook-Air.local>
2020-05-02 16:53:09 +0530

    Code cleanuo, created Card class

--- app.js
+++ app.js
@@ -49,27 +49,102 @@ let cards = [{
   pattern: '(?:2131|1800|35\d{3})\d{11}',
   issuerPattern: '(?:2131|1800|35\d{3})',
 }];
+class Card {
+  constructor(props) {
+    if (!props) props = {};
+    Object.assign(this, {
+      name: props.name,
+      _number: props.number || props._number,
+      cvv: props.cvv,
+      expiry: props.expiry,
+      issuer: props.issuer,
+    });
+  }
+  set number(number) {
+    this._number = number.replace(/[^\d]/g, '');
+  }
+  get number() {
+    return this._number;
+  }
+  hasValidNumber() {
+    const add = (sum, curr) => Number(sum) + Number(curr);
+    let sequence = this._number.split('');
+    const last = sequence.splice(-1);
+    // Double alternate digits starting from the right
+    sequence = sequence.reverse().map((val, idx) => {
+      if (idx % 2 === 0) {
+        val = (val * 2).toString().split('').reduce(add, 0);
+      }
+      return val;
+    });
+    // Add all digits in sequence and multiply by 9
+    const checkSum = (sequence.reduce(add, 0) * 9).toString();
+    if (checkSum[checkSum.length - 1] == last) return true;
+  }
+  isValidExpiry() {
+    const [month, year] = this.expiry.split('/');
+    const today = new Date();
+    const expiry = new Date();
+    if (+month < 1 || +month > 12) {
+      alert("Invalid Date");
+      return false;
+    }
+    expiry.setFullYear(Number('20' + year), Number(month) - 1, 1);
+  
+    if (expiry < today) {
+      alert("The expiry date is before today's date. Please select a valid expiry date");
+      return false;
+    }
+    return true;
+  }
+  addToStorage() {
+    // Adds card details to localStorage
+    let cardList = JSON.parse(window.localStorage.getItem("cardList") || "[]");
+    if (cardList.find(card => this.number === card.number)) {
+      alert('Card already exists');
+      return false;
+    }
+    cardList.push(this);
+    window.localStorage.setItem('cardList', JSON.stringify(cardList));
+    return true;
+  }
+}
 
-let issuingCard = '';
+function getElById(id) {
+  return document.getElementById(id);
+}
+
+function formatter(event, el) {
+  let str = el.value;
+  if (event.keyCode === 8) {
+    if (['/', ' '].includes(str[str.length - 1])) el.value = str.slice(0, -1);
+    return;
+  }
+}
 
 function addNewCard(button) {
-  const formElement = document.getElementById('form1');
+  // Opens up Form, hides 'Add New' button
+  const formElement = getElById('form1');
   formElement.style.display = 'block';
   button.style.display = 'none';
 }
+
 function cancelAddOp() {
-  const formElement = document.getElementById('form1');
-  const addBtn = document.getElementById('newCardBtn');
+  // Closes form, makes 'Add New' button visible again
+  const formElement = getElById('form1');
+  const addBtn = getElById('newCardBtn');
   formElement.style.display = 'none';
   addBtn.style.display = 'inline-block';
 }
+
 function deleteFuncGenerator(cardToDel) {
+  // Returns function to delete a card with closure over a card's info
   return function deleteCard() {
+    // Finds and deletes card from cardList, refreshes list
     const res = window.prompt("Are you sure you want to delete this card? 'Y' or 'N'");
-    console.log(res);
     if (res && (res == 'Y' || res == 'y')) {
       let cardList = JSON.parse(localStorage.getItem("cardList") || "[]");
-      let idx = cardList.findIndex(card => card.cardNo === cardToDel.cardNo);
+      let idx = cardList.findIndex(card => card._number === cardToDel.number);
       if (idx > -1) {
         cardList.splice(idx, 1);
         localStorage.setItem('cardList', JSON.stringify(cardList));
@@ -83,16 +158,23 @@ function deleteFuncGenerator(cardToDel) {
   }
 }
 function listCards() {
-  let ul = document.getElementById('card-list');
+  // Refreshes/Creates Cards list
+  let ul = getElById('card-list');
   ul.innerHTML = '';
   let cardList = JSON.parse(localStorage.getItem("cardList") || "[]");
   if (!cardList.length) {
-    let li = createListElement('No cards found! <br /> Add a new card to the list', '', false);
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    span.setAttribute('id', 'empty-list-msg');
+    span.innerHTML = 'No cards found! <br /> Add a new card to the list';
+    li.appendChild(span);
     li.style.textAlign = 'center';
     ul.appendChild(li);
   }
   for (let card of cardList) {
-    let li = createListElement(card.name, card.cardNo, true, deleteFuncGenerator(card));
+    card = new Card(card);
+    let li = createCardElement(card, true);
+    // Add issuer logo to every card
     const logo = document.createElement('img');
     Object.assign(logo, {
       src: `assets/${card.issuer}-logo.png`,
@@ -103,105 +185,77 @@ function listCards() {
     ul.appendChild(li);
   }
 }
-function createListElement(text, subText, canDelete, deleteFn) {
+function createCardElement(card, canDelete) {
+  /* 
+    Creates a 'li' element to display within list 
+    takes 'canDelete' as param to add a delete control to the 'li' element
+  */
+  let { name, number, expiry } = card;
+  let spanWithText = (text, id, cl) => {
+    let span = document.createElement('span');
+    if (id) span.setAttribute('id', id);
+    if (cl) span.setAttribute('class', cl);
+    if (text) span.innerHTML = text;
+    return span;
+  };
+
   let li = document.createElement('li');
   li.setAttribute('id', 'card-item');
-  if (text) {
-    let textSpan = document.createElement('span');
-    textSpan.setAttribute('id', 'name-detail');
-    textSpan.innerHTML = text;
-    li.appendChild(textSpan);
-  }
-  if (subText) {
-    let subTextSpan = document.createElement('span');
-    subTextSpan.setAttribute('id', 'num-detail');
-    subTextSpan.innerHTML = subText;
-    li.appendChild(subTextSpan);
-  }
+  
+  li.appendChild(spanWithText(name, 'name-detail'));
+  li.appendChild(spanWithText(number, 'num-detail'));
+  li.appendChild(spanWithText(`Valid Till: <b>${expiry}</b>`, 'expiry-detail'));
+  
   if (canDelete) {
     let deleteBtn = document.createElement('button');
     Object.assign(deleteBtn, {
       id: 'delete-btn',
       innerText: 'Delete Card',
-      onclick: deleteFn,
+      onclick: deleteFuncGenerator(card),
     });
     li.appendChild(deleteBtn);
   }
   return li;
 }
-function addToStorage(cardDetails) {
-  let cardList = JSON.parse(localStorage.getItem("cardList") || "[]");
-  if (cardList.find(card => card.cardNo === cardDetails.cardNo)) {
-    alert('Card already exists');
-    return false;
-  }
-  cardList.push(cardDetails);
-  localStorage.setItem('cardList', JSON.stringify(cardList));
-  return true;
-}
-function validateAndSubmit(event) {
-  var form = document.getElementById('form1');
-  let cardDetails = {};
-  ['cardNo', 'name', 'expiry', 'cvv'].forEach(field => {
-    cardDetails[field] = form[field].value;
-  })
-  cardDetails.cardNo = cardDetails.cardNo.replace(/[^\d]/g, '');
-  cardDetails.issuer = issuingCard.issuer;
-  if (!isValidCard(cardDetails.cardNo)) {
-    alert('Invalid Card Number!');
-    event.preventDefault();
-    return false;
-  }
-  if (!isValidExpiry(cardDetails.expiry)) {
-    event.preventDefault();
-    return false;
-  }
-  const added = addToStorage(cardDetails);
-  if (!added) {
-    event.preventDefault();
-    return false;
-  }
-  listCards();
-}
-function isValidExpiry(str) {
-  const [month, year] = str.split('/');
-  const today = new Date();
-  const expiry = new Date();
-  if (+month < 1 || +month > 12) {
-    alert("Invalid Date");
-    return false;
-  }
-  expiry.setFullYear(Number('20' + year), Number(month) - 1, 1);
 
-  if (expiry < today) {
-    alert("The expiry date is before today's date. Please select a valid expiry date");
+function validateAndSubmit(event) {
+  // Form validations, Call to Store card, List refresh
+  try {
+    var form = getElById('form1');
+    newCard = new Card();
+    ['number', 'name', 'expiry', 'cvv'].forEach(field => {
+      newCard[field] = form[field].value;
+    })
+    newCard.issuer = issuingCard.issuer;
+    if (
+      !issuingCard
+      || !RegExp(`^${issuingCard.pattern}$`).test(newCard.number)
+      || !newCard.hasValidNumber()
+    ) {
+      alert('Invalid Card Number!');
+      event.preventDefault();
+      return false;
+    }
+    if (!newCard.isValidExpiry()) {
+      event.preventDefault();
+      return false;
+    }
+    console.log(newCard);
+    const added = newCard.addToStorage();
+    if (!added) {
+      event.preventDefault();
+      return false;
+    }
+    listCards();
+  } catch(e) {
+    console.log('ERROR: ', e);
+    event.preventDefault();
     return false;
   }
-  return true;
 }
-function isValidCard(cardNo) {
-  if (!issuingCard || !RegExp(`^${issuingCard.pattern}$`).test(cardNo)) return false;
-  const add = (sum, curr) => Number(sum) + Number(curr);
-  let sequence = cardNo.split('');
-  const last = sequence.splice(-1);
-  sequence = sequence.reverse().map((val, idx) => {
-    if (idx % 2 === 0) {
-      val = (val * 2).toString().split('').reduce(add, 0);
-    }
-    return val;
-  });
-  const checkSum = (sequence.reduce(add, 0) * 9).toString();
-  if (checkSum[checkSum.length - 1] == last) return true;
-}
-function formatter(event, el) {
-  let str = el.value;
-  if (event.keyCode === 8) {
-    if (['/', ' '].includes(str[str.length - 1])) el.value = str.slice(0, -1);
-    return;
-  }
-}
+
 function dateFormatter(event) {
-  var expiryEl = document.getElementById('expiry');
+  var expiryEl = getElById('expiry');
   var str = expiryEl.value;
   var v = str.replace(/[^\d]/g, '');
   v = v.replace(/.{2}/g, function (a) {
@@ -209,6 +263,7 @@ function dateFormatter(event) {
   }).slice(0, 5);
   expiryEl.value = v;
 }
+
 function setIssuer(cardNumber) {
   let first4 = cardNumber.slice(0, 5);
   let issueCard = '';
@@ -219,10 +274,10 @@ function setIssuer(cardNumber) {
       break;
     }
   }
-  let issuerEl = document.getElementById('issuer-logo');
+  let issuerEl = getElById('issuer-logo');
   if (issueCard) {
     cardNumField.maxLength = issueCard.cardLength + 3; // '+3' to account for spaces
-    let cvvInput = document.getElementById('cvv');
+    let cvvInput = getElById('cvv');
     cvvInput.maxLength = issueCard.cvvLen;
     cvvInput.minLength = issueCard.cvvLen;
     issuerEl.setAttribute('src', `assets/${issueCard.issuer}-logo.png`);
@@ -233,6 +288,7 @@ function setIssuer(cardNumber) {
   }
   return issueCard;
 }
+
 function cardFormatter(event) {
   str = cardNumField.value;
   if (!str) {
@@ -247,13 +303,16 @@ function cardFormatter(event) {
   });
   cardNumField.value = v;
 }
+
 function toggleInputs(disable) {
-  let expiry = document.getElementById('expiry');
-  let cvv = document.getElementById('cvv');
-  let submitBtn = document.getElementById('submit-btn');
+  let expiry = getElById('expiry');
+  let cvv = getElById('cvv');
+  let submitBtn = getElById('submit-btn');
   expiry.disabled = disable;
   cvv.disabled = disable;
   submitBtn.disabled = disable;
 }
-var cardNumField = document.getElementById('cardNo');
+let issuingCard = '';
+let newCard;
+var cardNumField = getElById('number');
 listCards();
